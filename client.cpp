1e446e8a8d8518d215e967f8d7e33bd5f249ef97
xglight <xglight0@gmail.com>
2024-07-09 11:43:38 +0800

    v1.0

--- client.cpp
+++ client.cpp
@@ -1,51 +1,80 @@
+#include <cstring>
+#include <handleapi.h>
 #include <iostream>
+#include <minwindef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "winsock2.h"
 using namespace std;
 const int port = 8888;
+char recvbuf[1505];
+SOCKET sockClient;
+DWORD WINAPI get_message(LPVOID lpParam) {
+    while (1) {
+        memset(recvbuf, 0, sizeof(recvbuf));
+        int result = recv(sockClient, recvbuf, sizeof(recvbuf), 0);
+        if (result == 0) {
+            printf("服务端已断开\n");
+            closesocket(sockClient);
+            // WSACleanup();
+            break;
+        } else if (result > 0)
+            printf("%s\n", recvbuf);
+        else {
+            printf("服务端出现错误，已断开\n");
+            closesocket(sockClient);
+            break;
+        }
+    }
+    return 0;
+}
+char username[1024], ip[1024];
+char message[1505], ch;
+WSADATA wsaData;
+SOCKADDR_IN addrSrv;
 int main() {
-    WSADATA wsaData;
-    char buff[1024];
-    memset(buff, 0, sizeof(buff));
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("初始化Winsock失败");
+        printf("初始化失败");
         return 0;
     }
-
-    SOCKADDR_IN addrSrv;
+    printf("请输入用户名：");
+    scanf("%s", username);
+    printf("请输入服务器IP地址：");
+    scanf("%s", ip);
     addrSrv.sin_family = AF_INET;
-    addrSrv.sin_port = htons(port);                        //端口号
-    addrSrv.sin_addr.S_un.S_addr = inet_addr("10.0.9.31"); //IP地址
+    addrSrv.sin_port = htons(port);
+    addrSrv.sin_addr.S_un.S_addr = inet_addr(ip);
 
-    //创建套接字
-    SOCKET sockClient = socket(AF_INET, SOCK_STREAM, 0);
+    sockClient = socket(AF_INET, SOCK_STREAM, 0);
     if (SOCKET_ERROR == sockClient) {
-        printf("Socket() error:%d", WSAGetLastError());
+        printf("Socket连接失败:%d", WSAGetLastError());
         return 0;
     }
-
-    //向服务器发出连接请求
     if (connect(sockClient, (struct sockaddr *)&addrSrv, sizeof(addrSrv)) == INVALID_SOCKET) {
         printf("连接失败:%d", WSAGetLastError());
         return 0;
     } else {
-        //接收数据
-        recv(sockClient, buff, sizeof(buff), 0);
-        printf("%s\n", buff);
+        recv(sockClient, recvbuf, sizeof(recvbuf), 0);
+        printf("%s\n", recvbuf);
     }
-
-    //发送数据
-    char buffs[] = "";
-    //不断输入，然后发送
-    while (true) {
-        cin >> buffs;
-        send(sockClient, buffs, sizeof(buffs), 0);
+    send(sockClient, username, sizeof(username), 0);
+    HANDLE hThread = CreateThread(NULL, 0, get_message, NULL, 0, NULL);
+    CloseHandle(hThread);
+    cin.getline(message, sizeof(message));
+    while (1) {
+        bool f = 0;
+        while ((ch = getchar()) != EOF) {
+            f = 1;
+            if (ch == '\n') break;
+            sprintf(message, "%s%c", message, ch);
+        }
+        if (!f) break;
+        if (strcmp(message, "") == 0 || strcmp(message, "\n") == 0) continue;
+        send(sockClient, message, sizeof(message), 0);
+        memset(message, 0, sizeof(message));
     }
-
-    //关闭套接字
     closesocket(sockClient);
-    WSACleanup();    //释放初始化Ws2_32.dll所分配的资源。
-    system("pause"); //让屏幕暂留
+    WSACleanup();
+    system("pause");
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,58 +1,98 @@
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <minwindef.h>
+#include <psdk_inc/_socket_types.h>
+#include <set>
 #include "winsock2.h"
 using namespace std;
-char recvBuf[100];
-SOCKET sockConn;
-DWORD WINAPI get_message(LPVOID lpParamter) {
-    while (true) {
+int cnt = 1;
+set<int> id_free, id_have;
+map<char *, int> client;
+SOCKET sockcli[1005];
+char recvBuf[1505], sendBuf[1505];
+DWORD WINAPI client_server(LPVOID lpParamter) {
+    char username[1005];
+    sprintf(username, "%s", (char *)lpParamter);
+    int id = client[(char *)lpParamter];
+    while (1) {
         memset(recvBuf, 0, sizeof(recvBuf));
-        recv(sockConn, recvBuf, sizeof(recvBuf), 0);
-        printf("%s\n", recvBuf);
+        int result = recv(sockcli[id], recvBuf, sizeof(recvBuf), 0);
+        if (result == 0) {
+            printf("服务端：客户端%s已断开\n", username);
+            closesocket(sockcli[id]);
+            client[(char *)lpParamter] = 0;
+            id_free.insert(id), id_have.erase(id);
+            // WSACleanup();
+            break;
+        } else if (result > 0) {
+            printf("%s:%s\n", username, recvBuf);
+            sprintf(sendBuf, "%s:%s", username, recvBuf);
+            for (auto i = id_have.begin(); i != id_have.end(); i++) {
+                if (*i == id) continue;
+                send(sockcli[*i], sendBuf, strlen(sendBuf), 0);
+            }
+        } else {
+            printf("服务端：客户端%s出现错误，已断开\n", username);
+            closesocket(sockcli[id]);
+            client[(char *)lpParamter] = 0;
+            id_free.insert(id), id_have.erase(id);
+            break;
+        }
     }
-    closesocket(sockConn);
+    return 0;
 }
 const int port = 8888; //端口
+char ip[1024];
+WSADATA wsaData;
+SOCKADDR_IN addrSrv;
+SOCKADDR_IN addrClient[1005];
+HANDLE hThread[1005];
 int main() {
-    WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("服务器建立失败！");
         return 0;
     }
     SOCKET sockSrv = socket(AF_INET, SOCK_STREAM, 0);
-    SOCKADDR_IN addrSrv;
     addrSrv.sin_family = AF_INET;
     addrSrv.sin_port = htons(port);
     addrSrv.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
     int retVal = bind(sockSrv, (LPSOCKADDR)&addrSrv, sizeof(SOCKADDR_IN));
     if (retVal == SOCKET_ERROR) {
-        printf("连接失败:%d\n", WSAGetLastError());
+        printf("socket连接失败:%d\n", WSAGetLastError());
         return 0;
     }
 
     if (listen(sockSrv, 10) == SOCKET_ERROR) {
-        printf("监听失败:%d", WSAGetLastError());
+        printf("socket监听失败:%d", WSAGetLastError());
         return 0;
     }
-
-    SOCKADDR_IN addrClient;
+    strcpy(ip, inet_ntoa(addrSrv.sin_addr));
+    printf("服务端启动成功，IP:[%s] 监听端口：%d\n", ip, port);
     int len = sizeof(SOCKADDR);
-
+    for (int i = 1; i <= 1000; i++)
+        id_free.insert(i);
     while (1) {
         //等待客户端
-        sockConn = accept(sockSrv, (SOCKADDR *)&addrClient, &len);
-        if (sockConn == SOCKET_ERROR) {
+        cnt = *id_free.begin();
+        sockcli[cnt] = accept(sockSrv, (SOCKADDR *)&addrClient[cnt], &len);
+        if (sockcli[cnt] == SOCKET_ERROR) {
             printf("%d", WSAGetLastError());
             break;
         }
-        printf("客户端IP:[%s]\n", inet_ntoa(addrClient.sin_addr));
-        char sendbuf[] = "你好，这里是服务端。";
-        int iSend = send(sockConn, sendbuf, sizeof(sendbuf), 0);
+        char sendbuf[] = "服务端：你好，这里是服务端。";
+        int iSend = send(sockcli[cnt], sendbuf, sizeof(sendbuf), 0);
         if (iSend == SOCKET_ERROR) {
-            printf("");
+            printf("服务端：有客户端连接，但是连接失败:%d", WSAGetLastError());
             break;
         }
-        HANDLE hThread = CreateThread(NULL, 0, get_message, NULL, 0, NULL);
-        CloseHandle(hThread);
+        recv(sockcli[cnt], recvBuf, sizeof(recvBuf), 0);
+        client[(char *)recvBuf] = cnt;
+        printf("连接到客户端:%s IP:[%s]\n", recvBuf, inet_ntoa(addrClient[cnt].sin_addr));
+        hThread[cnt] = CreateThread(NULL, 0, client_server, (LPVOID)recvBuf, 0, NULL);
+        CloseHandle(hThread[cnt]);
+        id_free.erase(cnt), id_have.insert(cnt);
     }
 
     closesocket(sockSrv);
